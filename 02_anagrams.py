a94fa63fb7783dd6f89fcde4f5abf80b57783e59
GI10 <tadus@op.pl>
2021-12-02 16:04:07 +0100

    Adding 02_anagrams.py

---
+++ 02_anagrams.py
@@ -0,0 +1,23 @@
+"""Zadanie 2: Anagramy¶
+Napisz funkcję, która znajdzie wszystkie anagramy danego słowa z podanej listy. Znalezione wyrazy należy zwrócić
+w postaci listy.
+
+from typing import List
+
+
+def anagrams(word: str, lst_of_words: List[str]) -> List[str]:
+    pass
+Wyjaśnienie: Czym jest anagram? Są to dwa wyrazy, które składają się z tych samych liter. Dla przykładu:
+
+'mata' & 'tama' == true
+'kat' & 'tap' == false
+'meta' & 'mata' == false
+'abba' & 'bbaa' == true
+Podpowiedź
+W zadaniu można wykorzystać Counter"""
+
+from typing import List
+
+
+def anagrams(word: str, lst_of_words: List[str]) -> List[str]:
+    pass
