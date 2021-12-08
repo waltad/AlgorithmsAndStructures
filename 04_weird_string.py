7d825a727ea598e7aacaabbcf1e58eed6d7ee7d9
GI10 <tadus@op.pl>
2021-12-08 13:28:37 +0100

    A second solution has been added.

--- 04_weird_string.py
+++ 04_weird_string.py
@@ -16,16 +16,34 @@ def to_weird_case(string: str) -> str:
     len_string = len(string)
     new_string = ''
     for i in range(0, len_string):
-        if i % 2 != 0:
-            new_string += string[i].lower()
-        else:
+        if i % 2 == 0:
             new_string += string[i].upper()
+        else:
+            new_string += string[i].lower()
+    return new_string
+
+
+def to_weird_case2(string: str) -> str:
+    i = 0
+    new_string = ''
+    for character in string:
+       if character != ' ':
+           if i % 2 == 0:
+               new_string += str.upper(character)
+           else:
+               new_string += character
+           i += 1
+       else:
+           i = 0
+           new_string += character
     return new_string
 
 
 if __name__ == '__main__':
     text = 'String'
     print(to_weird_case(text))
+    print(to_weird_case2(text))
     text = 'Algorytmy i struktury danych'
     print(to_weird_case(text))
+    print(to_weird_case2(text))
 
