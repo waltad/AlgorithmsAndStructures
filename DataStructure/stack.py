3592d524ad832cc3494a8c6bfb4a00dafb95b8e1
GI10 <tadus@op.pl>
2021-12-20 14:43:59 +0100

    Adding file stack.py

---
+++ DataStructure/stack.py
@@ -0,0 +1,120 @@
+"""Zadanie 1: Stos
+Napisz kod symulujący stos książek. Klasa powinna umożliwiać dodawanie oraz zdejmowanie książki ze stosu, przeglądanie
+tytułów w stosie oraz ostatniej pozycji na stosie. Użyj magic methods umożliwiających:
+
+dodawanie stosów do siebie
+porównywanie wielkości stosów
+reprezentację stringową stosu
+podanie długości stosu
+from typing import List
+
+class BooksStack:
+    def add_new_book(self, title: str):
+        pass
+    def get_book(self):
+        pass
+    def all_books(self) -> List[str]:
+        pass
+Magic methods
+Klasa powinna zaimplementować następujące magic methods, aby spełnić wymagania:
+
+
+def __init__(self):
+    pass
+
+def __add__(self, second_stack):
+    pass
+
+# comparision
+def __gt__(self, second_stack):
+    pass
+
+def __lt__(self, second_stack):
+    pass
+
+def __ge__(self, second_stack):
+    pass
+
+def __le__(self, second_stack):
+    pass
+
+def __str__(self):
+    pass
+
+def __repr__(self):
+    pass
+
+def __len__(self):
+    pass
+str vs repr
+Przykładowy kod wykorzystujący klasę BooksStack:
+
+
+my_books = BooksStack("Mój stos książek", "Przyrodnicze")
+
+my_books.add_new_book("Gepardy")
+my_books.add_new_book("Słonie")
+my_books.add_new_book("Kotki")
+
+print(my_books.all_books())
+print(my_books.get_book())
+print(my_books.all_books())
+
+her_books = BooksStack("Jej stos ksiażek", "Przyrodnicze")
+her_books.add_new_book("Konie")
+
+my_books = my_books + her_books
+print(my_books.all_books())
+
+print(my_books > her_books)
+print(my_books <= her_books)
+
+print(my_books)
+print(repr(my_books))
+
+print(len(my_books))
+Oraz pożądane wyjście:
+
+
+['Gepardy', 'Słonie', 'Kotki']
+Kotki
+['Gepardy', 'Słonie']
+['Gepardy', 'Słonie', 'Konie']
+True
+False
+Stack: Mój stos książek with category of books: Przyrodnicze
+stack_name: Mój stos książek
+ category: Przyrodnicze
+ books: Gepardy Słonie Konie
+3
+"""
+from typing import List
+
+
+class BooksStack:
+    def __init__(self, stack_name: str, category: str):
+        self.stack_name = stack_name
+        self.category = category
+        self.books: List[str] = []
+
+    def __str__(self):
+        return f"Stack: {self.stack_name} with category of books: {self.category}"
+
+    def add_new_book(self, title: str):
+        self.books.append(title)
+
+    def get_book(self):
+        pass
+
+    def all_books(self) -> List[str]:
+        return self.books
+
+
+if __name__ == '__main__':
+    my_books = BooksStack("Mój stos książek", "Przyrodnicze")
+
+    my_books.add_new_book("Gepardy")
+    my_books.add_new_book("Słonie")
+    my_books.add_new_book("Kotki")
+
+    print(my_books.all_books())
