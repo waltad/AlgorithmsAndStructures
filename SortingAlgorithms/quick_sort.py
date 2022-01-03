20c06b2449e8bfb80ef99f976a360f120b4fede3
GI10 <tadus@op.pl>
2022-01-03 14:51:45 +0100

    Adding file quick_sort.py

---
+++ SortingAlgorithms/quick_sort.py
@@ -0,0 +1,10 @@
+"""QuickSort
+Należy zaimplementować sortowanie metodą quick_sort:
+[QuickSort](http://www.algorytm.edu.pl/algorytmy-maturalne/quick-sort.html
+Sugestia:
+W celu przypomnienia sobie rekurencji, preferowane będzie rozwiązanie zadania przy jej pomocy."""
+from typing import List
+
+
+def quick_sort(lst: List[int]) -> List[int]:
+    pass
